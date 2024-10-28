92f199a4638e4a056f06511b96d4448e4c38f8ac
Rapheal Odejinmi <77197711+Rapheal555@users.noreply.github.com>
2024-10-28 20:53:27 +0100

    first commit

---
+++ src/components/Features.tsx
@@ -0,0 +1,96 @@
+import {
+  Title,
+  Text,
+  Container,
+  Grid,
+  GridCol,
+  Card,
+  Image,
+} from "@mantine/core";
+
+import classes from "@/styles/Introduction.module.css";
+
+export function Features() {
+  const features = [
+    {
+      title: "Data-Driven Decision-Making",
+      description: "Leverage data to drive informed business decisions.",
+      image: "/images/data.png",
+      span: 7,
+    },
+    {
+      title: "Sales Pipeline Tracking",
+      description: "Monitor and optimize your sales pipeline.",
+      image: "/images/sales.png",
+      span: 5,
+    },
+    {
+      title: "Manage Customer",
+      description: "Centralize customer information and interactions.",
+      image: "/images/data.png",
+      span: 4,
+    },
+    {
+      title: "Track Order History",
+      description: "Gain insights into your order history and trends.",
+      image: "/images/order.png",
+      span: 4,
+    },
+    {
+      title: "Predictive Analytics",
+      description: "Leverage data to predict future performance and trends.",
+      image: "/images/analytics.png",
+      span: 4,
+    },
+  ];
+  return (
+    <Container mt={25} className={classes.wrapper} size={"xl"}>
+      <Container mb={"lg"} size={"sm"} className={classes.inner}>
+        <Title fw={"bold"} className={classes.title}>
+          Exploring Features for Enhanced Success and Growth
+        </Title>
+
+        <Container p={0} size={600}>
+          <Text size="lg" className={classes.description}>
+            Cheetah is a sales management platform designed to help you grow
+            your business with ease.
+          </Text>
+        </Container>
+      </Container>
+
+      <Grid>
+        {features.map((feature, i) => (
+          <GridCol span={{ base: 12, md: 6, lg: feature.span }} key={i}>
+            <Card radius={15} bg={"#fafafa"}>
+              <Text fz={"xl"} fw={"bold"}>
+                {feature.title}
+              </Text>
+              <Text fz={"sm"}>{feature.description}</Text>
+              <Image
+                mt={"lg"}
+                maw={"250"}
+                mah={"250"}
+                className={classes.image}
+                src={feature.image}
+                alt="Cheetah"
+              />
+            </Card>
+          </GridCol>
+        ))}
+      </Grid>
+
+      <Container mb={"lg"} size={"sm"} className={classes.inner}>
+        <Title fw={"bold"} className={classes.title}>
+          Tailored POS Solutions for Your Unique Store
+        </Title>
+
+        <Container p={0} size={600}>
+          <Text size="lg" className={classes.description}>
+            Cheetah is a sales management platform designed to help you grow
+            your business with ease.
+          </Text>
+        </Container>
+      </Container>
+    </Container>
+  );
+}
---
+++ src/components/Introduction.tsx
@@ -0,0 +1,49 @@
+import { Title, Text, Button, Container, Image, Box } from "@mantine/core";
+// import { Dots } from './Dots';
+import classes from "@/styles/Introduction.module.css";
+
+export function Introduction() {
+  return (
+    <Container pb={0} className={classes.wrapper} size={"md"}>
+      {/* <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
+      <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
+      <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
+      <Dots className={classes.dots} style={{ right: 0, top: 60 }} /> */}
+
+      <Container size={"sm"} className={classes.inner}>
+        <Title className={classes.title}>
+          The Simple, Secure, and Scalable Way to{" "}
+          <Text component="span" className={classes.highlight} inherit>
+            Run Your Business
+          </Text>
+        </Title>
+
+        <Container p={0} size={600}>
+          <Text size="lg" c="dimmed" className={classes.description}>
+            Cheetah is a sales management platform designed to help you grow
+            your business with ease.
+          </Text>
+        </Container>
+
+        <Box mb={"xl"} className={classes.controls}>
+          <Button className={"primary-button"} size="lg">
+            Get Started
+          </Button>
+          <Button
+            className={classes.control}
+            size="lg"
+            variant="default"
+            color="gray"
+          >
+            Live Demo
+          </Button>
+        </Box>
+      </Container>
+      <Image
+        className={classes.image}
+        src={"/images/cheetah.png"}
+        alt="Cheetah Dashboard"
+      />
+    </Container>
+  );
+}
