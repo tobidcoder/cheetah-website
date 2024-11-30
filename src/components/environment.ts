86583f08a62b4c77ea24e83a4a68f3a674d0084b
Odejinmi Tobiloba <odetobi123@gmail.com>
2024-11-30 09:19:36 +0100

    some update

--- src/components/environment.ts
+++ src/components/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // URL: "https://app.usecheetah.com/api" //window.location.protocol + '//' + window.location.hostname,
-    URL: "http://127.0.0.1:8000/api" //window.location.protocol + '//' + window.location.hostname,
+    URL: "https://app.usecheetah.com/api" //window.location.protocol + '//' + window.location.hostname,
+    // URL: "http://127.0.0.1:8000/api" //window.location.protocol + '//' + window.location.hostname,
 };
