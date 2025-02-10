022e05a0c6ef0e37ff21223e454390553bf66289
Rapheal Odejinmi <odejinmirapheal@gmail.com>
2025-02-10 19:12:58 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-
 import { Features } from "@/components/Features";
 import { Feedback } from "@/components/Feedback";
 // import { Footer } from "@/components/Footer";
 import { GetStarted } from "@/components/GetStarted";
 import { Introduction } from "@/components/Introduction";
 import { Paper, Container } from "@mantine/core";
-import {News} from '@/components/News'
+import { News } from "@/components/News";
+import { Benefits } from "../components/Benefits";
 
 export default function HomePage() {
   return (
@@ -14,10 +14,10 @@ export default function HomePage() {
         <Paper pt={"lg"} bg={"#fafafa"} radius="lg">
           <Introduction />
         </Paper>
-
+        <Benefits />
         <Features />
         <Feedback />
-        <News/>
+        <News />
       </Container>
       <GetStarted />
     </>
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,52 +4,64 @@ import classes from "@/styles/Introduction.module.css";
 
 export function Introduction() {
   return (
-    <Container mt='xl' pb={0} className={classes.wrapper} size={"md"}>
+    <Container mt="xl" pb={0} className={classes.wrapper} size={"md"}>
       {/* <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
       <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
       <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
       <Dots className={classes.dots} style={{ right: 0, top: 60 }} /> */}
- 
+
       <Container size={"md"} className={classes.inner}>
         <Title className={classes.introTitle}>
-        FREE to sell anywhere easily, quickly, and seamlessly.
+          AI-powered retail inventory optimization.
           {/* <Text component="span" className={classes.highlight} inherit>
           
           </Text> */}
         </Title>
 
         <Container p={0} size={600}>
-          <Text size="lg" c='#052315' ta='center'  className={classes.desc}>
-          Custom-tailored product suites for <b>suppermarket, pharmacy, restaurants, retail, and beauty</b> businesses.
+          <Text size="lg" c="#052315" ta="center" className={classes.desc}>
+            Custom-tailored product suites for{" "}
+            <b>suppermarket, pharmacy, restaurants, retail, and beauty</b>{" "}
+            businesses.
           </Text>
         </Container>
 
-        <Box mb='xl' className={classes.controls}>
+        <Box mb="xl" className={classes.controls}>
           {/* <a href="http://" target="_blank" rel="noopener noreferrer"></a> */}
-          <a style={{margin:"12px"}} target="_blank" rel="noopener noreferrer" href="https://app.usecheetah.com/">
-          <Button
-            me={{ base: "0", lg: "lg" }}
-            mb={{ base: "lg", lg: "0" }}
-            className={"primary-button"}
-            w={{base:'100%'}}
-            size="lg"
-            radius="md"
+          <a
+            style={{ margin: "12px" }}
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://app.usecheetah.com/"
           >
-            Get Started FREE
-          </Button>
+            <Button
+              me={{ base: "0", lg: "lg" }}
+              mb={{ base: "lg", lg: "0" }}
+              className={"primary-button"}
+              w={{ base: "100%" }}
+              size="lg"
+              radius="md"
+            >
+              Get Started FREE
+            </Button>
           </a>
-          <a style={{margin:"12px"}} target="_blank" rel="noopener noreferrer" href="https://calendly.com/cheetahdemo/30min">
-          <Button
-            // mb={{ lg: "xs" }}
-            className={"secondary-button"}
-            w={{base:'100%'}}
-            size="lg"
-            variant="default"
-            color="gray"
-             radius="md"
+          <a
+            style={{ margin: "12px" }}
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://calendly.com/cheetahdemo/30min"
           >
-            Book A Demo
-          </Button>
+            <Button
+              // mb={{ lg: "xs" }}
+              className={"secondary-button"}
+              w={{ base: "100%" }}
+              size="lg"
+              variant="default"
+              color="gray"
+              radius="md"
+            >
+              Book A Demo
+            </Button>
           </a>
         </Box>
       </Container>
